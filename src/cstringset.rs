d0bbcf51eafdcebafddabfc8aea5f7a25fa31d22
Mike Dilger <mike@optcomp.nz>
2018-05-25 10:53:22 +1200

    CStringSet.iter()

---
+++ src/cstringset.rs
@@ -0,0 +1,35 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+use std::ptr;
+use std::slice::Iter;
+
+// A set of strings, prepared for a C FFI
+pub struct CStringSet {
+    // This is the pointer you pass to C
+    pub pp: *const *const c_char,
+    cstrings: Vec<CString>,
+    ptrs: Vec<*const c_char>,
+}
+
+impl CStringSet {
+    pub fn new(s: Vec<&str>) -> CStringSet {
+        let mut ss = CStringSet {
+            pp: ptr::null(),
+            cstrings: s.iter().map(|s| CString::new(&**s).unwrap()).collect(),
+            ptrs: vec![],
+        };
+
+        ss.ptrs = ss.cstrings.iter().map(|cs| cs.as_ptr()).collect();
+        ss.ptrs.push(ptr::null()); // C uses null ptr as terminator of ptr list
+        ss.pp = ss.ptrs.as_ptr();
+        ss
+    }
+
+    pub fn len(&self) -> usize {
+        self.cstrings.len()
+    }
+
+    pub fn iter(&self) -> Iter<CString> {
+        self.cstrings.iter()
+    }
+}
